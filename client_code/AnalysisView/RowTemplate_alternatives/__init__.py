167b65117e2a41fbb766d1693abc36bdc0140168
anvil.election653@passmail.net <anvil.election653@passmail.net>
2024-07-12 11:21:10 +0000

    Edited AnalysisView.RowTemplate_alternatives and Entrypoint

--- client_code/AnalysisView/RowTemplate_alternatives/__init__.py
+++ client_code/AnalysisView/RowTemplate_alternatives/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ...EditableLink import EditableLink
 
 
 class RowTemplate_alternatives(RowTemplate_alternativesTemplate):
@@ -12,3 +13,7 @@ class RowTemplate_alternatives(RowTemplate_alternativesTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.editable_link_1.add_event_handler('x-change-text', text)
+
+def change_text(self, text, **event_args):
+  anvil.server.call('change_cell_value_alternatives', self.item, text)
